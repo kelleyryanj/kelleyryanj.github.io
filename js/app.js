f410439bc7acd27e6a8c55b24d71b15e8df299d9
kelleyryanj <kelley.ryanj@gmail.com>
2019-06-23 13:58:36 -0400

    js

--- js/app.js
+++ js/app.js
@@ -27,17 +27,11 @@ function navOnClick() {
 }
 
 window.onclick = function(event) {
-    if(!event.target.matches('.dropButton')){
-        var dropdowns = document.getElementsByClassName("dropdownContent");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')){
-                openDropdown.classList.remove('show')
-            }
+      var dropdowns = document.getElementsByClassName("dropdownContent");
+      if(event.target =! dropdowns) {
+            dropdowns.style.display = "none";
         }
     }
-}
 
 function openTab(tabName) {
     var i, x;
